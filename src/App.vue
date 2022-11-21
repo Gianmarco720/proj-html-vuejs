7b8310160121951b805524cd221f15c38db5c4a6
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-11-21 11:27:45 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import AppHeader from './components/layout/AppHeader.vue'
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style lang="scss" scoped>
+
+</style>
