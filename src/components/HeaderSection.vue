4baae99b0386b672320ae547091276a249791d8f
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-11-21 13:25:25 +0100

    add header section

---
+++ src/components/HeaderSection.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+    name: 'HeaderSection'
+}
+</script>
+
+<template>
+    <section>
+        <div class="my_container">
+            <div class="row">
+                <div class="col-4">
+                    <h1 class="fw-bold text-white">SEM Campaigns Made Simple With Avada</h1>
+                    <p class="text-white">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure saepe
+                        veritatis nihil,
+                        consectetur soluta quo?</p>
+                    <a class="btn btn-warning rounded-pill text-uppercase fw-bold text-white" href="#" role="button">Buy
+                        avada
+                        now</a>
+                    <a class="btn btn-primary rounded-pill text-uppercase fw-bold ms-3" href="#" role="button">Contact
+                        us</a>
+                </div>
+                <div class="col-8"></div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/variables.scss' as *;
+
+section {
+    padding: 15% 0;
+
+    .btn-warning {
+        background-color: $seo-yellow-orange;
+    }
+
+    .btn-primary {
+        background-color: #556EED;
+    }
+}
+</style>
--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,9 +1,11 @@
 <script>
+import HeaderSection from '../HeaderSection.vue';
 import Navbar from '../Navbar.vue'
 export default {
     name: 'AppHeader',
     components: {
-        Navbar
+        Navbar,
+        HeaderSection
     }
 }
 </script>
@@ -12,12 +14,17 @@ export default {
     <header>
         <div class="my_container">
             <Navbar />
+            <HeaderSection />
         </div>
     </header>
 </template>
 
 <style lang="scss" scoped>
+@use '../../assets/scss/variables.scss' as *;
+
 header {
     background-image: url('../../assets/img/1-hero-image.png');
+    background-repeat: no-repeat;
+    background-size: cover;
 }
 </style>
